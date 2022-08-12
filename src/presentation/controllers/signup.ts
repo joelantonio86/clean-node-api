466571adf5be999396b2d1b3d45c8c47c4c6ff5b
Joel Antônio <joelantoniomg.86@gmail.com>
2022-08-11 22:49:25 -0300

    chore: correction husky and lint staged

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,16 +1,16 @@
 export class SignUpController {
-  handle(httpResquest: any): any {
+  handle (httpResquest: any): any {
     if (!httpResquest.body.name) {
       return {
         statusCode: 400,
-        body: new Error("Missing param: name"),
-      };
+        body: new Error('Missing param: name')
+      }
     }
     if (!httpResquest.body.email) {
       return {
         statusCode: 400,
-        body: new Error("Missing param: email"),
-      };
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
